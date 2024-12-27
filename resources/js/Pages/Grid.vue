b96569b274cc9bb9724eaad42cb9d7e0eda4f4b0
Pedro Alonso <palhm.99@gmail.com>
2024-12-27 00:13:40 -0600

    Avances con el grid

--- resources/js/Pages/Grid.vue
+++ resources/js/Pages/Grid.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Link } from "@inertiajs/vue3";
+import {ref} from "vue";
 
 interface GridColumn {
     label: string,
@@ -35,12 +36,44 @@ interface Props {
 
 defineProps<Props>()
 
+const query = ref<string>('')
+const timer = ref()
+
+const delay = 700
+
+
+function buscar(){
+
+
+}
+
 </script>
 
 <template>
     <app-layout>
         <div class="container mx-auto">
-            <h1 class="py-2 text-2xl text-blue-800 font-semibold my-4">{{ title }}</h1>
+            <div class="flex justify-between items-center">
+                <h1 class="py-2 text-2xl text-blue-800 font-semibold my-4">{{ title }}</h1>
+                <div class="w-[30rem] flex gap-4">
+                    <div class="flex items-center rounded shadow">
+                        <select name="columna" id="columna" class="rounded-l flex-shrink border-0 border-r border-stone-200">
+                            <option v-for="col of columns">{{ col.label }}</option>
+                        </select>
+                        <input type="text"
+                               placeholder="buscar"
+                               name="buscar"
+                               v-model="query"
+                               class="w-full flex-grow px-4 py-2 rounded-r border border-none"
+                               id="buscar">
+                    </div>
+                    <button
+                        @click.prevent="buscar"
+                        class="px-4 py-2 whitespace-nowrap bg-blue-800 rounded text-white">
+                        <i class="bi bi-search"></i>
+                        Buscar
+                    </button>
+                </div>
+            </div>
             <div class="flex">
                 <Link class="px-4 py-2 rounded bg-white shadow mb-2 border-b-4" v-for="l of toolbar.actions" :href="l.url">
                     <i :class="['bi',l.icon]"></i>
